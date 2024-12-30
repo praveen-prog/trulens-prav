ec969deddf1671af934a8d8ca333dca91830955a
Praveen Kumar Chandran <praveenhome@Praveens-MacBook-Pro.local>
2024-12-29 20:07:25 -0500

    set up script added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'snowflake-cortex-app',
+    version= '0.0.0',
+    author= 'Praveen',
+    author_email= 'praveenmek@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
